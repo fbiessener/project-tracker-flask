84e2a894986a7b45d22a13d1460e3a328506a021
Your Name <you@example.com>
2020-02-20 16:43:36 -0600

    Successfully adding new students to db from server/browser

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 """A web application for tracking projects, students, and student grades."""
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash
 import hackbright
 
 app = Flask(__name__)
+
+SECRET_KEY = 'ABC'
 
 
 @app.route('/student')
@@ -22,8 +24,25 @@ def get_student():
 
 @app.route('/search')
 def search_students():
-    
+
     return render_template('student_search.html')
+
+
+@app.route('/new-student', methods=['POST'])
+def new_student():
+
+    # first_name, last_name, github = request.form.get('new')
+
+    # hackbright.make_new_student(first_name, last_name, github)
+
+    first = request.form.get('first_name')
+    last = request.form.get('last_name')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first, last, github)
+
+    # flash('Successfully added student, try search now')
+    return render_template('student_info.html')
 
 
 if __name__ == '__main__':
